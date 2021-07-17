3e943dafc9dc88c2ff34dc511ec14021695a4592
Anton Pomozov <anton.pomozov@ooma.com>
2021-07-17 16:07:43 +0300

    Add hellper property 'anySource'

--- Sources/FeatureToggleKit/Source.swift
+++ Sources/FeatureToggleKit/Source.swift
@@ -10,7 +10,15 @@ import Foundation
 public protocol Source: AnyObject {
     associatedtype Feature
 
+    var anySource: AnySource<Feature> { get }
+
     func fetch(completion: @escaping (Result<[Feature], Swift.Error>) -> Void)
+}
+
+public extension Source {
+    var anySource: AnySource<Feature> {
+        AnySource(wrappedSource: self)
+    }
 }
 
 public class AnySource<Feature>: Source {
