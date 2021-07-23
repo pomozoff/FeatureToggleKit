f315816a8d1bd518d762c6fae65ddccfe214e5e0
Anton Pomozov <anton.pomozov@ooma.com>
2021-07-23 03:18:28 +0300

    Add test for provider

--- Tests/FeatureToggleKitTests/Tests/FeatureToggleKitTests.swift
+++ Tests/FeatureToggleKitTests/Tests/FeatureToggleKitTests.swift
@@ -39,6 +39,34 @@ final class FeatureToggleKitTests: XCTestCase {
         }
         wait(for: [expectation], timeout: Constants.defaultTimeout)
     }
+
+    func testProvider() {
+        let jsonSource = JSONSource<FeatureModel>(fileUrl: Bundle.module.url(forResource: "featuresList1", withExtension: "json")!)
+        let plistSource = PlistSource<FeatureModel>(fileUrl: Bundle.module.url(forResource: "featuresList2", withExtension: "plist")!)
+
+        let provider = Provider<FeatureModel>(
+            sources: [jsonSource.anySource, plistSource.anySource]
+        )
+
+        var counter = 0
+        let expectation = XCTestExpectation(description: "Fetch features from the provider")
+        expectation.expectedFulfillmentCount = 2
+
+        provider.fetch { result in
+            switch result {
+            case .success():
+                counter == 0
+                    ? XCTAssert(provider.isFeatureEnabled(name: "minus"), "Features is disabled")
+                    : XCTAssert(!provider.isFeatureEnabled(name: "minus"), "Features is enabled")
+                XCTAssert(!provider.isFeatureEnabled(name: "unknown feature"), "Features is enabled")
+            case let .failure(error):
+                XCTFail("Failure: \(error)")
+            }
+            expectation.fulfill()
+            counter += 1
+        }
+        wait(for: [expectation], timeout: Constants.defaultTimeout)
+    }
 }
 
 private enum Constants {
