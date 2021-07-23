82c859f1471568034a291f5477710cc8550f9e79
Anton Pomozov <anton.pomozov@ooma.com>
2021-07-23 03:18:18 +0300

    Simplify provider

--- Sources/FeatureToggleKit/Provider.swift
+++ Sources/FeatureToggleKit/Provider.swift
@@ -11,18 +11,16 @@ import Foundation
 /**
  The provider contains a list of feature sources where features could be fetched from.
  */
-public class Provider<Model: DecodableFeature, Storage: Feature> {
-    public init(sources: [AnySource<Model>], mapper: @escaping (Model) -> Storage?) {
+public class Provider<M: DecodableFeature> {
+    public init(sources: [AnySource<M>]) {
         self.sources = sources
-        self.mapper = mapper
     }
 
-    private let sources: [AnySource<Model>]
-    private let mapper: (Model) -> Storage?
+    private let sources: [AnySource<M>]
     private let featuresUpdateSemaphore = DispatchSemaphore(value: 1)
 
     @Atomic
-    private var features: [Storage: Bool] = [:]
+    private var features: [String: Bool] = [:]
 }
 
 extension Provider {
@@ -35,19 +33,17 @@ extension Provider {
      then the completion closure will be called three times. This allows a client to start
      updating UI as soon as the fastest source returns features.
      */
-    public func fetch(features: [Model], completion: @escaping (Result<Void, Error>) -> Void) {
+    public func fetch(completion: @escaping (Result<Void, Error>) -> Void) {
         for source in sources {
             source.fetch { [weak self] result in
                 guard let self = self else { return }
 
                 switch result {
-                case let .success(features):
+                case let .success(models):
                     self.featuresUpdateSemaphore.wait()
 
-                    self.features = features.reduce(into: self.features) { result, feature in
-                        self.mapper(feature).map {
-                            result[$0] = feature.isEnabled
-                        }
+                    self.features = models.reduce(into: self.features) { result, model in
+                        result[model.name] = model.isEnabled
                     }
 
                     self.featuresUpdateSemaphore.signal()
@@ -63,7 +59,7 @@ extension Provider {
      Check if the feature is enabled.
      If the feature is absent it is disabled.
      */
-    public func isFeatureEnabled(_ feature: Storage) -> Bool {
-        features[feature] ?? false
+    public func isFeatureEnabled(name: String) -> Bool {
+        features[name] ?? false
     }
 }
